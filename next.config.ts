98a1f206350acdb2f1ece3f7d32c1be10443e28c
Berk Karadalan <berkkaradalan@gmail.com>
2025-02-25 20:23:08 +0300

    Deploy

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  metadataBase: new URL('https://berkk-portfolio.github.io'),
+};
+
+export default nextConfig;
